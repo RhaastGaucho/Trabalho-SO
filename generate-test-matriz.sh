19d0cb7dc9edbcf2883a20efa97bf340f262481e
Mauricio <mauricio.rockembach@gmail.com>
2025-07-04 19:49:55 -0300

    ALterações

--- generate-test-matriz.sh
+++ generate-test-matriz.sh
@@ -6,34 +6,34 @@ echo $n
 
 A=()
 B=()
-for ((i=0; i<n*n; i++)); do
+
+for((i=0; i<n*n; i++)); do
     A+=($((RANDOM % 10)))
     B+=($((RANDOM % 10)))
 done
 
-opcoes=("a" "s" "m")
-op=${opcoes[$((RANDOM % 3))]}
+echo "A"
 
-echo "Matriz A:"
-
-for ((i=0; i<n; i++)); do
-    for ((j=0; j<n; j++)); do
+for((i=0; i<n; i++)); do
+    for((j=0; j<n; j++)); do
         idx=$((i * n + j))
         echo -n "${A[$idx]} "
     done
     echo
 done
 
-echo "Matriz B:"
+echo "B"
 
-for ((i=0; i<n; i++)); do
-    for ((j=0; j<n; j++)); do
+for((i=0; i<n; i++)); do
+    for((j=0; j<n; j++)); do
         idx=$((i * n + j))
         echo -n "${B[$idx]} "
     done
     echo
 done
 
+opcoes=("a" "s" "m")
+op=${opcoes[$((RANDOM % 3))]}
 echo $op
 
 entrada="$n"

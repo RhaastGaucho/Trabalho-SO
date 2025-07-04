88c0ed6187dc0edf0eac5eec1b3c199bd812e6fa
Mauricio <mauricio.rockembach@gmail.com>
2025-07-04 19:40:18 -0300

    Alteração no cout final para espaçar cada número

--- matriz.cpp
+++ matriz.cpp
@@ -39,7 +39,7 @@ int main() {
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            cout << R[i][j];
+            cout << R[i][j] << " ";
         }
         cout << endl;
     }
